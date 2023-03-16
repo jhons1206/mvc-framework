eecbcf039a7447b0413bbb540ad16d0995aaa5e8
jhons1206 <webdeveloper84@gmail.com>
2023-03-16 09:16:29 -0500

    Clase 08: Vistas II - Cómo pasarle parámetros

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controllers;
 
-class HomeController {
+class HomeController extends Controller {
 
     public function index() {
-        return "Hola desde la página de Inicio";
+
+        return $this->view('home');
     }
 }
