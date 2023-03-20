5593d98e788e7965d72481b560a2659d6af0cb20
Jhon Sandoval <webdeveloper84@gmail.com>
2023-03-20 17:25:13 -0500

    Clase 26: Buscador I - Avance

--- resources/views/contacts/index.php
+++ resources/views/contacts/index.php
@@ -11,29 +11,32 @@
 </head>
 
 <body>
-    <h1 class="text-2xl font-bold mb-2">Listado de Contactos</h1>
+    <div class="container mx-auto mt-5">
+        <h1 class="text-2xl font-bold mb-5">Listado de Contactos</h1>
 
-    <form action="" class="flex">
-        <input type="text">
-    </form>
+        <form action="" class="flex mb-5">
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Ingrese el contacto a buscar">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-1 rounded">Buscar</button>
+        </form>
 
-    <a href="/contacts/create">Crear contacto</a>
+        <a class="text-orange-700" href="/contacts/create">Crear contacto</a>
 
-    <ul class="list-disc list-inside">
-        <?php foreach ($contacts['data'] as $contact) : ?>
-            <li>
-                <a href="/contacts/<?= $contact['id'] ?>">
-                    <?= $contact['name'] ?>
-                </a>
-            </li>
-        <?php endforeach ?>
-    </ul>
+        <ul class="list-disc list-inside mt-2">
+            <?php foreach ($contacts['data'] as $contact) : ?>
+                <li>
+                    <a class="text-sky-700" href="/contacts/<?= $contact['id'] ?>">
+                        <?= $contact['name'] ?>
+                    </a>
+                </li>
+            <?php endforeach ?>
+        </ul>
 
-    <?php
+        <?php
         $paginate = 'contacts';
 
         require_once "../resources/views/assets/pagination.php";
-    ?>
+        ?>
+    </div>
 
 </body>
 
